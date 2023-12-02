4fcb0cb2b85aff6c77d8a63b6f2c99190fa2a625
tammysteph <dominas.tamara@gmail.com>
2023-12-02 19:59:11 +0100

    Added date function to app JS file

--- src/app.js
+++ src/app.js
@@ -1,1 +1,48 @@
-alert("Hello");
+function formatDate() {
+  let now = new Date();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[now.getMonth()];
+
+  let minutes = now.getMinutes();
+  let hours = now.getHours();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let formattedDate = `${day} ${hours}:${minutes}, ${month} ${now.getDate()}, ${now.getFullYear()}`;
+
+  let currentDay = document.querySelector("#current-day");
+  currentDay.innerHTML = formattedDate;
+}
+
+formatDate();
