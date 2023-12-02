c86c4ac6ce1fdfde89dcaaa71269852277831c20
tammysteph <dominas.tamara@gmail.com>
2023-12-02 20:38:42 +0100

    Added forecast days of the week function to the JS file

--- src/app.js
+++ src/app.js
@@ -46,3 +46,45 @@ function formatDate() {
 }
 
 formatDate();
+
+function forecastDays() {
+  let now = new Date();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let currentDayNumber = now.getDay();
+
+  let dayPlusOne = days[(currentDayNumber + 1) % 7];
+  let forecastDay1 = document.querySelector("#dayPlusOne");
+  forecastDay1.innerHTML = dayPlusOne;
+
+  let dayPlusTwo = days[(currentDayNumber + 2) % 7];
+  let forecastDay2 = document.querySelector("#dayPlusTwo");
+  forecastDay2.innerHTML = dayPlusTwo;
+
+  let dayPlusThree = days[(currentDayNumber + 3) % 7];
+  let forecastDay3 = document.querySelector("#dayPlusThree");
+  forecastDay3.innerHTML = dayPlusThree;
+
+  let dayPlusFour = days[(currentDayNumber + 4) % 7];
+  let forecastDay4 = document.querySelector("#dayPlusFour");
+  forecastDay4.innerHTML = dayPlusFour;
+
+  let dayPlusFive = days[(currentDayNumber + 5) % 7];
+  let forecastDay5 = document.querySelector("#dayPlusFive");
+  forecastDay5.innerHTML = dayPlusFive;
+
+  let dayPlusSix = days[(currentDayNumber + 6) % 7];
+  let forecastDay6 = document.querySelector("#dayPlusSix");
+  forecastDay6.innerHTML = dayPlusSix;
+}
+
+forecastDays();
